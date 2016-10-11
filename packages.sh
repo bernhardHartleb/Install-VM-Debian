2fb7797043fa3bb528d04143ee4a49cbe3318e9b
Bernhard Hartleb <bernhard.hartleb@gmail.com>
2016-10-11 14:19:07 +0200

    Adapt list of packages necessary for Linux development

--- packages.sh
+++ packages.sh
@@ -1,61 +1,59 @@
 #!/bin/bash
 # Adrian Remonda 2013
+# Bernhard Hartleb 2016
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 
-echo "### Installing basic stuff..."
-sudo apt-get install -y software-properties-common #to get apt-add repository
-sudo apt-get install -y build-essential autoconf  vim vim-gnome meld  git gitk git-email git-gui  tree > /dev/null
+echo "### Installing basic tools"
+# Allows us to add debian repositories
+sudo apt-get install -y software-properties-common curl > /dev/null
 
-echo "### Installing ssh"
+echo "### Installing ssh server"
 sudo apt-get install -y openssh-server > /dev/null
 
 echo "### Installing versioning tools"
-sudo apt-get install -y mercurial libapache2-svn subversion meld > /dev/null
-sudo apt-get install -y p7zip-full zip unzip unrar p7zip xutils-dev > /dev/null
+sudo apt-get install -y git gitk git-gui mercurial subversion > /dev/null
+
+echo "### Installing compression tools"
+sudo apt-get install -y zip unzip unrar-free p7zip xarchiver > /dev/null
 
 echo "### Installing vim"
-sudo apt-get install -y vim vim-gnome exuberant-ctags unrar-free > /dev/null
+sudo apt-get install -y vim vim-gnome exuberant-ctags > /dev/null
 
-echo "### Installing kernel stuff"
-sudo apt-get install -y aptitude gparted colorgcc bison flex texinfo gnuplot gnuplot-x11 > /dev/null
+echo "### Installing compiling tools"
+sudo apt-get install -y build-essential gcc gdb autoconf ncurses-dev xutils-dev colorgcc > /dev/null
 
-echo "### Installing compiling tools..."
-sudo apt-get install -y ncurses-dev  > /dev/null
-sudo apt-get install -y libncurses5-dev > /dev/null
+echo "### Installing kernel tools"
+sudo apt-get install -y linux-tools bison flex texinfo gnuplot gnuplot-x11 > /dev/null
 
-echo "### Installing misc tools..."
-sudo apt-get install -y gparted minicom  > /dev/null
-sudo apt-get install -y xarchiver  > /dev/null
-sudo apt-get install -y xclip > /dev/null
+echo "### Installing networking tools"
+sudo apt-get install -y wireshark > /dev/null
 
-echo "### Python"
-sudo apt-get install -y python python-serial python3 python-setuptools python-gpgme
-sudo apt-get install -y python-matplotlib
+echo "### Installing misc tools"
+sudo apt-get install -y gparted minicom > /dev/null
 
+echo "### Installing Python"
+sudo apt-get install -y python python3 python-serial python-setuptools python-gpgme > /dev/null
+sudo apt-get install -y python-matplotlib > /dev/null
 
-echo "### Installing x86 toolchain. Necesesary for xilinx tools and bb kernel"
+echo "### Installing Go"
+sudo apt-get install -y golang > /dev/null
+
+echo "### Installing x86 toolchain. Necesesary for Xilinx tools and bb kernel"
 sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 zlib1g:i386
+sudo apt-get -y update > /dev/null
+sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 zlib1g:i386
 
-echo "kernel and eliux stuff"
-sudo apt-get install device-tree-compiler lzma lzop u-boot-tools
+echo "### Installing kernel and embedded linux tools"
+sudo apt-get install -y device-tree-compiler debootstrap lzma lzop u-boot-tools
 
-#sudo apt-get install -y nemo    # mejor que nautilus porque tiene F3
+#echo "### Installing Qt5 development libraries"
+#sudo apt-get install qt5-default
 
-#Para que abra el ternmial con nautilus
-#sudo apt-get install -y nautilus-open-terminal
-
-# guake, para abrir la consola con F12
-sudo apt-get install -y guake
-
-echo "###Installing QT"
-echo "###  TODO test. Qt parece que lo instala alguien mas"
-sudo apt-get install -y qt4-dev-tools qt5-default
-
-
-# ma
-apt-get install caja-open-terminal mate-extras
+# Personal preference
+echo "### Installing optional console tools"
+sudo apt-get install -y tree meld xclip > /dev/null
+sudo apt-get install -y nautilus-open-terminal > /dev/null
+#sudo apt-get install mate-extras guake
